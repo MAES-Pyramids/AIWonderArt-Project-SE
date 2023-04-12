e25b4a01519661e169230f760dba487154b913da
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-04-12 03:04:52 +0200

    start impelementing post and dalle routes

--- Server/app.js
+++ Server/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 
+const postRouter = require("./routes/postRoutes");
+const dalleRouter = require("./routes/dalleRoutes");
 //-------------------------------------------//
 const app = express();
 //---------------middleware------------------//
@@ -9,6 +11,9 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json({ limit: "50mb" }));
 //-----------------Routes--------------------//
+app.use("/api/v1/Post", postRouter);
+app.use("/api/v1/Dalle", dalleRouter);
+
 app.get("/", async (req, res) => {
   res.status(200).json({
     status: "success",
---
+++ Server/models/post.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+//-------------------Schema----------------//
+const postsSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  prompt: { type: String, required: true },
+  photo: { type: String, required: true },
+});
+//--------------------Model------------------//
+const Post = mongoose.model("Post", postsSchema);
+//--------------------Export-----------------//
+module.exports = Post;
