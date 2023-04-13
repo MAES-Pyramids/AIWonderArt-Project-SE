55351f5450409bcf0954907ed8e2982e3fbf8fdf
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-04-13 23:44:00 +0200

    impelementing post controller functiond (get and post)

--- Server/controllers/postController.js
+++ Server/controllers/postController.js
@@ -1,7 +1,40 @@
-const { v2, cloudinary } = require("cloudinary");
+const { v2: cloudinary } = require("cloudinary");
+const Post = require("./../models/post");
 //--------------cloudinary Config-------------//
-
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 //---------------Controller------------------//
-exports.getRequest = (req, res, next) => {};
+exports.getRequest = async (req, res, next) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Fetching posts failed, please try again",
+    });
+  }
+};
 
-exports.postRequest = async (req, res) => {};
+exports.postRequest = async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
+
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Unable to create a post, please try again",
+    });
+  }
+};
