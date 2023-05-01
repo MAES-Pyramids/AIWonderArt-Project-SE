d5e6de9795238cdc5ba0ce2c7fc8616cfb954ecf
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-05-01 20:04:52 +0300

    Fixs #2, After refactoring

--- Server/models/users.js
+++ Server/models/users.js
@@ -1,63 +1,63 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 //-----------------------------------------//
 const userSchema = new mongoose.Schema(
   {
     name: {
-      type: "string",
-      required: [true, "A user must have a name"],
+      type: 'string',
+      required: [true, 'A user must have a name']
     },
     email: {
-      type: "string",
-      required: [true, "A user must have an email"],
+      type: 'string',
+      required: [true, 'A user must have an email'],
       unique: true,
       lowercase: true,
-      validate: [validator.isEmail, "Please provide a valid email"],
+      validate: [validator.isEmail, 'Please provide a valid email']
     },
     password: {
-      type: "string",
-      required: [true, "A user must have a password"],
+      type: 'string',
+      required: [true, 'A user must have a password'],
       minlength: 8,
-      select: false,
+      select: false
     },
     passwordConfirm: {
-      type: "string",
-      required: [true, "A user must have a password confirmation"],
+      type: 'string',
+      required: [true, 'A user must have a password confirmation'],
       // This only works on CREATE and SAVE!!!
       validate: {
         validator: function(el) {
           return el === this.password;
         },
-        message: "Passwords are not the same!",
-      },
+        message: 'Passwords are not the same!'
+      }
     },
     passwordChangedAt: {
       type: Date,
-      select: false,
+      select: false
     },
     passwordResetToken: String,
-    passwordResetExpires: Date,
+    passwordResetExpires: Date
   },
   {
     toJSON: { virtuals: true },
-    toObject: { virtuals: true },
+    toObject: { virtuals: true }
   }
 );
 //-------------------Managing Password------------------//
 // Encrypt the password before saving it to the database
 // Note: data will get check by validator before it gets to this document middleware
-userSchema.pre("save", async function(next) {
+userSchema.pre('save', async function(next) {
   // Only run this function if password was actually modified
-  if (!this.isModified("password")) return next();
+  if (!this.isModified('password')) return next();
   // Hash the password with cost of 12
   this.password = await bcrypt.hash(this.password, 12);
   // Delete passwordConfirm field
   this.passwordConfirm = undefined;
 });
 // Set the passwordChangedAt property to the current time before saving the document
-userSchema.pre("save", function(next) {
-  if (!this.isModified("password") || this.isNew) return next();
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password') || this.isNew) return next();
   this.passwordChangedAt = Date.now() - 1000;
   next();
 });
@@ -78,6 +78,6 @@ userSchema.methods.isPasswordChanged = function(JWTtimeStamp) {
   return false;
 };
 //--------------------Model------------------//
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 //--------------------Export-----------------//
 module.exports = User;
